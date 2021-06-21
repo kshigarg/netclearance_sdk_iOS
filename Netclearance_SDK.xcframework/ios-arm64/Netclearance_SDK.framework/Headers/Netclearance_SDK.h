12d525ca7936ff1639020a3f0698217728d142bd
Rushabh Champnaeri <rushabh.champaneri@yahoo.com>
2021-06-21 17:36:16 +0530

    Added XCFramework

---
+++ Netclearance_SDK.xcframework/ios-arm64/Netclearance_SDK.framework/Headers/Netclearance_SDK.h
@@ -0,0 +1,35 @@
+/**********************************************************************
+*                                                                     *
+*Netclearance,Inc CONFIDENTIAL AND PROPRIETARY                        *
+*                                                                     *
+* COPYRIGHT (C) Netclearance Systems,Inc                 *
+*                                                                     *
+* ALL RIGHTS RESERVED BY Netclearance Systems,Inc . THIS PROGRAM MUST      *
+* BE USED SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY        *
+* Netclearance Systems,Inc . NO PART OF THIS PROGRAM MAY BE REPRODUCED     *
+* OR DISCLOSED TO OTHERS, IN ANY FORM, WITHOUT THE PRIOR WRITTEN      *
+* PERMISSION OF Netclearance Systems,Inc .                               *
+*                                                                     *
+*USE OF COPYRIGHT NOTICE DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.*
+*                                                                     *
+* Netclearance Systems,Inc CONFIDENTIAL AND PROPRIETARY                        *
+*                                                                     *
+**********************************************************************/
+/*
+ * Title: Netclearance_SDK.h
+ * Description : Header file for SDK
+ * Revision History
+ * Owner: Rushabh Champaneri                            Date: 01/06/2021                                                 Reason for change
+ **********************************************************************/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Netclearance_SDK.
+FOUNDATION_EXPORT double Netclearance_SDKVersionNumber;
+
+//! Project version string for Netclearance_SDK.
+FOUNDATION_EXPORT const unsigned char Netclearance_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Netclearance_SDK/PublicHeader.h>
+
+
